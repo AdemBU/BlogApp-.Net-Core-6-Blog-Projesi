53957e6df4df655a05ca8d40248c039049713064
Adem Çok <ademcok0693@gmail.com>
2025-11-01 08:50:15 +0300

    Identity Yapılanması

---
+++ Blog.Data/Mapping/RoleMap.cs
@@ -0,0 +1,65 @@
+﻿using Blog.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Mapping
+{
+    public class RoleMap : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            // Primary key
+            builder.HasKey(r => r.Id);
+
+            // Index for "normalized" role name to allow efficient lookups
+            builder.HasIndex(r => r.NormalizedName).HasName("RoleNameIndex").IsUnique();
+
+            // Maps to the AspNetRoles table
+            builder.ToTable("AspNetRoles");
+
+            // A concurrency token for use with the optimistic concurrency checking
+            builder.Property(r => r.ConcurrencyStamp).IsConcurrencyToken();
+
+            // Limit the size of columns to use efficient database types
+            builder.Property(u => u.Name).HasMaxLength(256);
+            builder.Property(u => u.NormalizedName).HasMaxLength(256);
+
+            // The relationships between Role and other entity types
+            // Note that these relationships are configured with no navigation properties
+
+            // Each Role can have many entries in the UserRole join table
+            builder.HasMany<AppUserRole>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
+
+            // Each Role can have many associated RoleClaims
+            builder.HasMany<AppRoleClaim>().WithOne().HasForeignKey(rc => rc.RoleId).IsRequired();
+
+            builder.HasData(new AppRole
+            {
+                Id = Guid.Parse("99173599-18B0-4271-B2C9-1267003053A7"),
+                Name = "Superadmin",
+                NormalizedName = "SUPERADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new AppRole
+            {
+                Id = Guid.Parse("C10FA6B4-A853-40FA-BA1A-1A84E25577D3"),
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new AppRole
+            {
+                Id = Guid.Parse("53EB31B9-071E-4782-961C-D95D9BADE071"),
+                Name = "User",
+                NormalizedName = "USER",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            }
+            );
+        }
+    }
+}
---
+++ Blog.Data/Mapping/UserRoleMap.cs
@@ -0,0 +1,34 @@
+﻿using Blog.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Mapping
+{
+    public class UserRoleMap : IEntityTypeConfiguration<AppUserRole>
+    {
+        public void Configure(EntityTypeBuilder<AppUserRole> builder)
+        {
+            // Primary key
+            builder.HasKey(r => new { r.UserId, r.RoleId });
+
+            // Maps to the AspNetUserRoles table
+            builder.ToTable("AspNetUserRoles");
+
+            builder.HasData(new AppUserRole
+            {
+                UserId = Guid.Parse("0DD20017-ED70-471F-9701-926A8F764EF2"),
+                RoleId = Guid.Parse("99173599-18B0-4271-B2C9-1267003053A7"),
+            },
+            new AppUserRole
+            {
+                UserId = Guid.Parse("5F04DF46-AA62-4381-B723-B80DED53A3A0"),
+                RoleId = Guid.Parse("C10FA6B4-A853-40FA-BA1A-1A84E25577D3"),
+            });
+        }
+    }
+}
