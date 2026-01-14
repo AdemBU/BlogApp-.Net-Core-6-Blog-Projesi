83afbcb5f47d1dbf47ed7dd8722a499983176c54
Adem Çok <ademcok0693@gmail.com>
2026-01-14 17:08:24 +0300

    Kullanıcı Profili, Şifresi ve Fotoğrafı Düzenleme

--- Blog.Entity/Entities/AppUser.cs
+++ Blog.Entity/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Blog.Core.Entities;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace Blog.Entity.Entities
 {
-    public class AppUser : IdentityUser<Guid>
+    public class AppUser : IdentityUser<Guid>, IEntityBase
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public Guid ImageId { get; set; } = Guid.Parse("487F2D55-22DE-46DD-BA51-44920C3BEC7A");
+        public Guid ImageId { get; set; } = Guid.Parse("44f4ffa6-f4ab-4128-8887-4f985357e587");
         public Image Image { get; set; }
         public ICollection<Article> Articles { get; set; }
     }
--- Blog.Web/Areas/Admin/Controllers/UserController.cs
+++ Blog.Web/Areas/Admin/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using Blog.Data.UnitOfWorks;
 using Blog.Entity.DTOs.Article;
 using Blog.Entity.DTOs.Users;
 using Blog.Entity.Entities;
+using Blog.Entity.Enums;
 using Blog.Service.Extensions;
+using Blog.Service.Helpers.Images;
 using Blog.Web.ResultMessages;
 using FluentValidation;
 using Microsoft.AspNetCore.Identity;
@@ -20,14 +23,20 @@ namespace Blog.Web.Areas.Admin.Controllers
         private readonly RoleManager<AppRole> _roleManager;
         private readonly IMapper _mapper;
         private readonly IToastNotification _toast;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IImageHelper _imageHelper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public UserController(UserManager<AppUser> userManager, IValidator<AppUser> validator, RoleManager<AppRole> roleManager, IMapper mapper, IToastNotification toast)
+        public UserController(UserManager<AppUser> userManager, IValidator<AppUser> validator, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager, IImageHelper imageHelper, IMapper mapper, IUnitOfWork unitOfWork, IToastNotification toast)
         {
             _userManager = userManager;
             _validator = validator;
             _roleManager = roleManager;
             _mapper = mapper;
             _toast = toast;
+            _signInManager = signInManager;
+            _imageHelper = imageHelper;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task<IActionResult> Index()
@@ -156,5 +165,88 @@ namespace Blog.Web.Areas.Admin.Controllers
             return NotFound();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var getImage = await _unitOfWork.GetRepository<AppUser>().GetAsync(x => x.Id == user.Id, x => x.Image);
+            var map = _mapper.Map<UserProfileDto>(user);
+            map.Image.FileName = getImage.Image?.FileName;
+
+            return View(map);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(UserProfileDto userProfileDto)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            if (ModelState.IsValid)
+            {
+                var isVerified = await _userManager.CheckPasswordAsync(user, userProfileDto.CurrentPassword);
+                // Şifre değişikliği isteniyorsa ve yeni şifre boş değilse
+                if (isVerified && userProfileDto.NewPassword != null && userProfileDto.Photo != null)
+                {
+                    var result = await _userManager.ChangePasswordAsync(user, userProfileDto.CurrentPassword, userProfileDto.NewPassword);
+                    // Şifre değişikliği başarılı ise ChangePasswordAsync metodu hem şifreyi hem de güvenlik damgasını (security stamp) günceller
+                    if (result.Succeeded)
+                    {
+                        // Hem şifre hem de profil bilgileri güncelleniyor
+                        await _userManager.UpdateSecurityStampAsync(user);
+                        await _signInManager.SignOutAsync();
+                        await _signInManager.PasswordSignInAsync(user, userProfileDto.NewPassword, true, false);
+
+                        user.FirstName = userProfileDto.FirstName;
+                        user.LastName = userProfileDto.LastName;
+                        user.PhoneNumber = userProfileDto.PhoneNumber;
+
+                        var imageUpload = await _imageHelper.Upload($"{userProfileDto.FirstName} {userProfileDto.LastName}", userProfileDto.Photo, ImageType.User);
+                        Image image = new(imageUpload.FullName, userProfileDto.Photo.ContentType, user.Email);
+                        await _unitOfWork.GetRepository<Image>().AddAsync(image);
+                        user.ImageId = image.Id;
+
+                        await _userManager.UpdateAsync(user);
+
+                        await _unitOfWork.SaveChangesAsync();
+
+                        _toast.AddSuccessToastMessage("Şifreniz ve bilgilerini başarıyla değiştirildi.");
+                        return View();
+                    }
+                    else
+                    {
+                        // Şifre değişikliği başarısız ise hata mesajlarını ekle
+                        result.AddToIdentityModelState(ModelState);
+                        return View();
+                    }
+                }
+                else if (isVerified && userProfileDto.Photo != null)
+                {
+                    // Sadece profil bilgileri güncelleniyor
+                    await _userManager.UpdateSecurityStampAsync(user);
+                    user.FirstName = userProfileDto.FirstName;
+                    user.LastName = userProfileDto.LastName;
+                    user.PhoneNumber = userProfileDto.PhoneNumber;
+
+                    var imageUpload = await _imageHelper.Upload($"{userProfileDto.FirstName} {userProfileDto.LastName}", userProfileDto.Photo, ImageType.User);
+                    Image image = new(imageUpload.FullName, userProfileDto.Photo.ContentType, user.Email);
+                    await _unitOfWork.GetRepository<Image>().AddAsync(image);
+                    user.ImageId = image.Id;
+
+                    await _userManager.UpdateAsync(user);
+                    await _unitOfWork.SaveChangesAsync();
+
+                    _toast.AddSuccessToastMessage("Bilgileriniz başarıyla değiştirildi.");
+                    return View();
+                }
+                else
+                {
+                    // Mevcut şifre yanlışsa hata mesajı göster
+                    _toast.AddErrorToastMessage("Bilgileriniz güncellenirken hata oluştu");
+                    return View();
+                }
+            }
+
+            return View();
+        }
     }
 }
